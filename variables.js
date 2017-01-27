7e51af50f56f183bf9d3fcb3ec8078c156bc00b6
Polly G <pollymugure@gmail.com>
2017-01-27 11:41:28 +0300

    Working with Variables

---
+++ variables.js
@@ -0,0 +1,10 @@
+//Variables
+var myName ="Polly G";
+var x = 10;
+var y = 11;
+x+y;
+
+//Arrays
+var num  = [20, 3 , 6, 8,10];
+ var numSorted = num.sort(function(x,y){return x-y});
+for(var i = 0; i<numSorted.length;i++){console.log(num[i])};
